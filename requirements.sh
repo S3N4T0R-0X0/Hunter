4055202d7c829e2c619d816fc336557d158a226b
S3N4T0R <121706460+S3N4T0R-0X0@users.noreply.github.com>
2025-04-20 05:34:24 -0800

    Add files via upload

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y build-essential
+sudo apt-get install -y libstdc++-dev
+sudo apt-get install -y libpcap-dev
+sudo apt-get install -y libssl-dev libcrypto++-dev
+sudo apt-get install -y libpthread-stubs0-dev
+sudo apt-get install -y libcomerr-dev
+sudo apt-get install -y libprocps-dev
+sudo apt-get install -y mingw-w64
+sudo apt-get clean
